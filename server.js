1707a004465e0ba8e41e37c009180afa53a4cf18
Carolina Batista <carolinabatista@192.168.1.14>
2025-03-28 15:50:18 -0300

    cors

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://wize-frontend.vercel.app" }));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
