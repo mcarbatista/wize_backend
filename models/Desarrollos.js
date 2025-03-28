bd066ecea1daa6959076d4d83b2dab0653645670
Carolina Batista <carolinabatista@192.168.1.14>
2025-03-28 14:46:26 -0300

    adding precio con formato

--- models/Desarrollos.js
+++ models/Desarrollos.js
@@ -34,4 +34,29 @@ const DesarrollosSchema = new mongoose.Schema({
     Updated_Date: Date
 }, { timestamps: true, collection: "desarrollos" });
 
+// 1) Pre-save: whenever we do doc.save()
+DesarrollosSchema.pre("save", function (next) {
+    // 'this' is the actual document about to be saved
+    if (typeof this.Precio === "number") {
+        // e.g. "100000" -> "100,000"
+        this.Precio_Con_Formato = this.Precio.toLocaleString("es-ES");
+    } else {
+        this.Precio_Con_Formato = "";
+    }
+    next();
+});
+
+// 2) Pre-findOneAndUpdate: whenever we do findOneAndUpdate() or findByIdAndUpdate()
+DesarrollosSchema.pre("findOneAndUpdate", function (next) {
+    // 'this' is the Mongoose query
+    const update = this.getUpdate();
+    if (update && typeof update.Precio === "number") {
+        update.Precio_Con_Formato = update.Precio.toLocaleString("es-ES");
+    } else if (update && update.$set && typeof update.$set.Precio === "number") {
+        // In case the update is nested inside $set
+        update.$set.Precio_Con_Formato = update.$set.Precio.toLocaleString("es-ES");
+    }
+    next();
+});
+
 module.exports = mongoose.model("Desarrollo", DesarrollosSchema);
